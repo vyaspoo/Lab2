736f74dd7044757b5555f11a253993b2f8ba31a8
vyaspoo <55698938+vyaspoo@users.noreply.github.com>
2020-01-15 00:29:08 -0500

    Update 2.js

---
+++ Pooja/2.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/57eaeb9578748ff92a000009/train/javascript
+function sumMix(x)
+{
+
+  let r=0;
+  for(let n of x)
+  {
+    r +=parseInt(n);
+  }
+  return r;
+  
+}
