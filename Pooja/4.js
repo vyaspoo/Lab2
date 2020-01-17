2f770056cf9a9b3ce7ee8b03056d06a5150f5459
vyaspoo <55698938+vyaspoo@users.noreply.github.com>
2020-01-17 18:42:47 -0500

    Update 4.js

---
+++ Pooja/4.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/javascript
+function getChar(c){
+  // ...
+var r = c;
+if(c>=32 || c<=126){
+var t = String.fromCharCode(c)
+return t
+}
+}
